2d9a913e5fe12fd7ebf7f20898e2fc265a3e8e6b
Natanael Bezerra <nsbbezerra@hotmail.com>
2022-09-21 14:09:03 -0300

    remove ssrcache

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,10 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { customTheme } from "../styles/theme";
 import "../styles/style.css";
-import { ssrCache, client } from "../lib/urql";
+import { client } from "../lib/urql";
 import { Provider } from "urql";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  if (pageProps.urqlState) {
-    ssrCache.restoreData(pageProps.urqlState);
-  }
-
   return (
     <Provider value={client}>
       <ChakraProvider theme={customTheme}>
